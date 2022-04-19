e6889335b7379c8707b606952671a36270775591
Richard Whitehill <whiteric@jlab.org>
2022-04-19 15:55:16 -0500

    update HW 4 prob 4

--- HW4/prob4.py
+++ HW4/prob4.py
@@ -48,7 +48,7 @@ def gen_plot(f,T,N,save_path,nodes='uniform'):
     ax[1].set_ylabel(r'$e(x)$',size=20)
     ax[0].tick_params(axis='both',which='major',labelsize=20,direction='in')
     ax[1].tick_params(axis='both',which='major',labelsize=20,direction='in')
-    ax[0].legend(fontsize=20)
+    ax[0].legend(fontsize=20,frameon=False)
     plt.tight_layout()
     plt.savefig(save_path,bbox_inches='tight')
 
@@ -61,6 +61,30 @@ if __name__ == '__main__':
     gen_plot(f,T,N,'fig1.png',nodes='uniform')
     gen_plot(f,T,N,'fig2.png',nodes='chebyshev')
     
+    uniform_nodes   = []
+    chebyshev_nodes = []
+    for n in N:
+            uniform_nodes.append(np.linspace(-1.0,1.0,n+1))
+            chebyshev_nodes.append(np.array([np.cos( (2*j+1)*np.pi / 2 / (n+1) ) for j in range(n+1)]))
+    
+    fix,ax = plt.subplots(nrows=1,ncols=1,figsize=(7,5))
+    for i in range(len(N)):
+        y = -1*np.ones(N[i] + 1) + i
+        y1 = y - 0.1
+        y2 = y + 0.1
 
+        ax.plot(uniform_nodes[i],y1,'k.')
+        ax.plot(chebyshev_nodes[i],y2,'r.')
+    ax.plot([],[],'k.',label='uniform')
+    ax.plot([],[],'r.',label='chebyshev')
+    for i in range(len(N)):
+        ax.text(1.1,-1+i,r'$n=%d$'%N[i],fontsize=20)
+    ax.set_xlim(right=1.5)
+    ax.set_ylim(top=2.0)
+    ax.set_xlabel(r'$x$',size=20)
+    ax.set_ylabel(r'$y$',size=20)
+    ax.tick_params(axis='both',which='major',labelsize=20,direction='in')
+    ax.legend(loc='upper center',fontsize=15)
+    plt.savefig('fig3.png',bbox_inches='tight')
 
     
