e816c2abf3e067c7a01d859099a938ab0a4f0e90
Richard Whitehill <whiteric@jlab.org>
2022-04-21 19:33:38 -0500

    update HW 4

--- HW4/prob5.py
+++ HW4/prob5.py
@@ -8,16 +8,20 @@ rcParams['text.usetex']         = True
 rcParams['font.family']         = 'sans-serif'
 rcParams['font.sans-serif']     = ['Helvetica']
 
+import csv
+from scipy.interpolate import interp1d
+
 def spline(dataX,dataY,t):
     a,b,c,d = spline_coeff(dataX,dataY)
     for i in range(len(dataX)-1):
         if dataX[i] <= t and dataX[i+1] >= t:
+            k = i
             break
-    xk = dataX[i]
-    y = a[i] + b[i]*(t-xk) + c[i]*(t-xk)**2.0 + d[i]*(t-xk)**3.0
+    xk = dataX[k]
+    y = a[k] + b[k]*(t-xk) + c[k]*(t-xk)**2.0 + d[k]*(t-xk)**3.0
     return y
 
-def spline_coeff(dataX,dataY):
+def spline_coeff(dataX,dataY,printA=False):
     n = len(dataX) - 1 
     h = np.zeros(n)
     for i in range(n):
@@ -29,8 +33,8 @@ def spline_coeff(dataX,dataY):
         A[i,i-1] = h[i-1]
         A[i,i+1] = h[i]
         A[i,i]   = 2.0*(h[i]+h[i-1])
-    bb = np.zeros([n+1,1])
-    for i in range(n):
+    bb = np.zeros(n+1)
+    for i in range(1,n):
         bb[i] = 3.0/h[i]*(dataY[i+1]-dataY[i]) - 3.0/h[i-1]*(dataY[i]-dataY[i-1])
     c = np.linalg.solve(A,bb)
     a = np.copy(dataY)
@@ -44,4 +48,34 @@ def spline_coeff(dataX,dataY):
     return a,b,c,d 
 
 if __name__ == '__main__':
+
+    file = open('prob5.txt') 
+
+    csvreader = csv.reader(file)
+    header = []
+    header = next(csvreader)
+
+    x_data = []
+    y_data = []
+    for row in csvreader:
+        x_data.append(float(row[0]))
+        y_data.append(float(row[1]))
+    file.close()
+    
+    x_data = np.array(x_data)
+    y_data = -1.0 * np.array(y_data)
+
+    T = np.linspace(min(x_data),max(x_data),100)
+    reconstruct = np.array([spline(x_data,y_data,t) for t in T])
+    f = interp1d(x_data,y_data,kind='cubic')
+
+    a,b,c,d = spline_coeff(x_data,y_data,True)
+    #for i in range(len(a)):
+    #    print('%.5f  %.5f  %.5f  %.5f'%(a[i],b[i],c[i],d[i]))
+
+    fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(7,5))
+    ax.plot(T,reconstruct,'c')
+    ax.plot(x_data,y_data,'k*')
+    ax.tick_params(axis='both',which='major',labelsize=20,direction='in')
+    plt.savefig('./fig4.png',bbox_inches='tight')
     
